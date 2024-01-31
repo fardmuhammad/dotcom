17f06403bfe79f55d5399c36c3cbb58c64a1c0fe
Fard Muhammad <heyitsfard@gmail.com>
2024-01-31 11:38:15 -0800

    Update Work.tsx
    We want work that isn't from a defunct business.

--- src/components/sections/Work.tsx
+++ src/components/sections/Work.tsx
@@ -1,7 +1,7 @@
 /**
  * TO-DO: Once you have work samples that are: 
  * — not proprietary (DKG)
- * — from a business that no longer exists (Liquidus)
+ * — not from a business that no longer exists (Liquidus)
  * — or that isn't in a dead format like Flash (anything from before 2010) 
  * 
  * Put it in the work section.
